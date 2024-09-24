56ec9be6925d7d4f9a34177463430a5a5a743d9b
StefanChannel <stefan@channel.surf>
2024-09-24 16:30:54 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "PonyDebugger",
+    platforms: [
+        .iOS(.v5)
+    ],
+    products: [
+        .library(
+            name: "PonyDebugger",
+            targets: ["PonyDebugger"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/square/SocketRocket.git", from: "0.5.1")
+    ],
+    targets: [
+        .target(
+            name: "PonyDebugger",
+            dependencies: ["SocketRocket"],
+            path: "ObjC",
+            sources: ["DerivedSources", "PonyDebugger"],
+            publicHeadersPath: "PonyDebugger",
+            cSettings: [
+                .headerSearchPath("DerivedSources"),
+                .define("ENABLE_NS_ASSERTIONS", to: "YES")
+            ],
+            linkerSettings: [
+                .linkedFramework("CoreData"),
+                .linkedFramework("CoreGraphics")
+            ]
+        )
+    ]
+)
